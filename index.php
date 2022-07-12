cebd1878d5e6bf382b38bc672f57a85a76a64701
Brian Kariuki <107594995+DeveloperAstra@users.noreply.github.com>
2022-07-12 18:17:05 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MPESA Testing</title>
+</head>
+<body>
+	<div>
+		<div style="align-content: center;">
+			<form action="pay.php" method="POST">
+				<input type="text" name="name" placeholder="Name" required>
+				<input type="tel" name="phone" placeholder="Phone" required>
+				<button type="submit" name="paynow">Pay</button>
+			</form>
+		</div>
+	</div>
+</body>
+</html>
---
+++ pay.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+if (isset($_POST['paynow'])) {
+	$phone = $_POST['phone'];
+	$name = $_POST['name'];
+
+        
+        $ch = curl_init('https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Authorization: Basic cFJZcjZ6anEwaThMMXp6d1FETUxwWkIzeVBDa2hNc2M6UmYyMkJmWm9nMHFRR2xWOQ==']);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        echo $response;
+
+
+	echo("Hello " . $name . '<br>Your Phone number is ' . $phone . ' and your access token is ' . $response);
+}else {
+	header('Location: index.php');
+}
+?>
